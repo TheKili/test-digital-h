e35a6011560b13bd4069bac8d95d01c5ecafa394
Kilian Ruess <kili.ruess@web.de>
2023-10-01 16:39:47 +0200

    adding badges, conditions and tests. Technically, the specification given in the task is not covering all cases. Normally, I would ask back and highlight those inconsitencies. This time, I decided to follow the instructions as they were given

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,10 +4,19 @@ import { User } from './types/user.interface';
 
 describe('getUsersBadge', () => {
 
+  it(`get God like`, async function () {
+    expect(getUsersBadge(getUserMock(2001))).toEqual(Icon.BADGE_GODLIKE);
+    expect(getUsersBadge(getUserMock(10000))).toEqual(Icon.BADGE_GODLIKE);
+  });
+
+  it(`get Platinum`, async function () {
+    expect(getUsersBadge(getUserMock(101))).toEqual(Icon.BADGE_PLATINUM);
+    expect(getUsersBadge(getUserMock(2000))).toEqual(Icon.BADGE_PLATINUM);
+  });
+
   it(`get Gold`, async function () {
     expect(getUsersBadge(getUserMock(100))).toEqual(Icon.BADGE_GOLD);
     expect(getUsersBadge(getUserMock(50))).toEqual(Icon.BADGE_GOLD);
-    expect(getUsersBadge(getUserMock(1000000))).toEqual(Icon.BADGE_GOLD);
   });
 
   it(`get Silver`, async function () {
@@ -22,10 +31,19 @@ describe('getUsersBadge', () => {
     expect(getUsersBadge(getUserMock(10))).toEqual(Icon.BADGE_BRONZE);
   });
 
+  it(`get Starter`, async function () {
+    expect(getUsersBadge(getUserMock(2))).toEqual(Icon.BADGE_STARTER);
+    expect(getUsersBadge(getUserMock(4))).toEqual(Icon.BADGE_STARTER);
+  });
+
+  it(`get Bad ass`, async function () {
+    expect(getUsersBadge(getUserMock(-1))).toEqual(Icon.BADGE_BADASS);
+    expect(getUsersBadge(getUserMock(-42342))).toEqual(Icon.BADGE_BADASS);
+  });
+
   it(`get no Icon`, async function () {
-    expect(getUsersBadge(getUserMock(4))).toEqual(null);
-    expect(getUsersBadge(getUserMock(-100))).toEqual(null);
     expect(getUsersBadge(getUserMock(0))).toEqual(null);
+    expect(getUsersBadge(getUserMock(1))).toEqual(null);
   });
 
 });
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ import { Icon } from './types/icon.enum';
 
 export const getUsersBadge = ( user: User ): Icon | null => {
   const {solutionCount} = user;
+
+  if ( solutionCount > 2000 ) return Icon.BADGE_GODLIKE;
+  if ( solutionCount > 100 )  return Icon.BADGE_PLATINUM;
   if ( solutionCount >= 50 )  return Icon.BADGE_GOLD;
   if ( solutionCount >= 25 )  return Icon.BADGE_SILVER;
   if ( solutionCount >= 5)    return Icon.BADGE_BRONZE;
+  if ( solutionCount > 1)     return Icon.BADGE_STARTER;
+  if ( solutionCount < 0)     return Icon.BADGE_BADASS;
   return Icon.DEFAULT ;
 
 };
