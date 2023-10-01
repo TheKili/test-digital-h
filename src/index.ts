30e3a343ce4f1f894a9ccd418036dc44924b573b
Kilian Ruess <kili.ruess@web.de>
2023-10-01 13:44:20 +0200

    adding return statement to each switch condition making component stateless

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,18 @@ import { User } from './types/user.interface';
 import { Icon } from './types/icon.enum';
 
 export const getUsersBadge = ( user: User ): Icon | null => {
-  let badge = null;
+
   switch ( true ) {
     case ( user.solutionCount >= 50 ):
-      badge = Icon.BADGE_GOLD;
+      return Icon.BADGE_GOLD;
       break;
     case ( user.solutionCount >= 25 ):
-      badge = Icon.BADGE_SILVER;
+      return Icon.BADGE_SILVER;
       break;
     case ( user.solutionCount >= 5):
-      badge = Icon.BADGE_BRONZE;
+      return Icon.BADGE_BRONZE;
       break;
     default:
-      badge = Icon.DEFAULT ;
+      return Icon.DEFAULT ;
    }
-  return badge;
 };
