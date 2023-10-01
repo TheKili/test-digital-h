02a948da92b4ced5720d5dcdae569135eb55779a
Kilian Ruess <kili.ruess@web.de>
2023-10-01 13:41:53 +0200

    recucing if statement complexity by re-ordering categories

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,17 @@ import { Icon } from './types/icon.enum';
 export const getUsersBadge = ( user: User ): Icon | null => {
   let badge = Icon.DEFAULT;
   switch ( true ) {
-    case ( user.solutionCount >= 5 && user.solutionCount < 25 ):
-      badge = Icon.BADGE_BRONZE;
-      break;
-    case ( user.solutionCount >= 25 && user.solutionCount < 50 ):
-      badge = Icon.BADGE_SILVER;
-      break;
     case ( user.solutionCount >= 50 ):
       badge = Icon.BADGE_GOLD;
       break;
+    case ( user.solutionCount >= 25 ):
+      badge = Icon.BADGE_SILVER;
+      break;
+    case ( user.solutionCount >= 5):
+      badge = Icon.BADGE_BRONZE;
+      break;
+
+
   }
   return badge;
 };
