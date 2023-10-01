b8c5db9aedbd13d1d9c496f10cdd427edd8fb5e4
Kilian Ruess <kili.ruess@web.de>
2023-10-01 16:19:27 +0200

    addin async funtion in function definition and to jest index.spec.ts

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,25 +4,25 @@ import { User } from './types/user.interface';
 
 describe('getUsersBadge', () => {
 
-  it(`get Gold`, function () {
+  it(`get Gold`, async function () {
     expect(getUsersBadge(getUserMock(100))).toEqual(Icon.BADGE_GOLD);
     expect(getUsersBadge(getUserMock(50))).toEqual(Icon.BADGE_GOLD);
     expect(getUsersBadge(getUserMock(1000000))).toEqual(Icon.BADGE_GOLD);
   });
 
-  it(`get Silver`, function () {
+  it(`get Silver`, async function () {
     expect(getUsersBadge(getUserMock(25))).toEqual(Icon.BADGE_SILVER);
     expect(getUsersBadge(getUserMock(49))).toEqual(Icon.BADGE_SILVER);
     expect(getUsersBadge(getUserMock(30))).toEqual(Icon.BADGE_SILVER);
   });
 
-  it(`get Bronze`, function () {
+  it(`get Bronze`, async function () {
     expect(getUsersBadge(getUserMock(5))).toEqual(Icon.BADGE_BRONZE);
     expect(getUsersBadge(getUserMock(24))).toEqual(Icon.BADGE_BRONZE);
     expect(getUsersBadge(getUserMock(10))).toEqual(Icon.BADGE_BRONZE);
   });
 
-  it(`get no Icon`, function () {
+  it(`get no Icon`, async function () {
     expect(getUsersBadge(getUserMock(4))).toEqual(null);
     expect(getUsersBadge(getUserMock(-100))).toEqual(null);
     expect(getUsersBadge(getUserMock(0))).toEqual(null);
